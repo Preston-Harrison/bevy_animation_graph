d7b4e22e7e375646e7ef6f241a0290765bc7392e
Preston Harrison <preston@prestonharrison.com>
2023-04-28 17:26:27 -0700

    node tree

--- src/animator.rs
+++ src/animator.rs
@@ -3,29 +3,35 @@ use std::{collections::HashMap, time::Duration};
 
 use crate::{animation::Animation, texture_iterator::TextureIterator};
 
+#[derive(Component)]
 pub struct Animator {
     animations: HashMap<String, Animation>,
-    default_animation: Animation,
-    active_animation: Option<String>,
-    frame: TextureIterator,
-    frame_start: Duration,
+    frame_start: Option<Duration>,
 }
 
+// TODO remove some of the panics here
 impl Animator {
-    pub fn new(default_animation: Animation, current_time: Res<Time>) -> Self {
+    pub fn new() -> Self {
         Animator {
             animations: HashMap::default(),
-            frame: TextureIterator::new(default_animation.first(), default_animation.last()),
-            default_animation,
-            active_animation: None,
-            frame_start: current_time.elapsed(),
+            frame_start: None,
         }
     }
 
-	pub fn add_animation(&mut self, name: String, animation: Animation) {
-		// TODO not panic here
-		assert!(!self.animations.contains_key(&name));
+    pub fn add_animation(&mut self, name: String, animation: Animation) {
+        assert!(!self.animations.contains_key(&name));
+        self.animations.insert(name, animation);
+    }
 
-		self.animations.insert(name, animation);
-	}
+    pub fn play(&mut self, name: String) {
+        // Return early if the animation is already playing.
+        if self.active == name {
+            return;
+        }
+
+        let animation = self.animations.get(&name).expect("animation not found");
+        self.frame = TextureIterator::new(animation.first(), animation.last());
+        self.active = name;
+        self.frame_start = None;
+    }
 }
---
+++ src/node_tree.rs
@@ -0,0 +1,101 @@
+use std::collections::HashMap;
+use crate::animation::Animation;
+
+macro_rules! unwrap_or_return {
+    ( $e:expr, $v:expr ) => {
+        match $e {
+            Some(x) => x,
+            None => return $v,
+        }
+    }
+}
+
+enum Condition {
+    Gt(String, String),
+    Lt(String, String),
+    Eq(String, String),
+}
+
+impl Condition {
+    fn eval(&self, variables: &HashMap<String, f32>) -> bool {
+        match self {
+            Condition::Gt(a, b) => {
+                match (variables.get(a), variables.get(b)) {
+                    (Some(v1), Some(v2)) => v1 > v2,
+                    _ => false
+                }
+            },
+            Condition::Lt(a, b) => {
+                match (variables.get(a), variables.get(b)) {
+                    (Some(v1), Some(v2)) => v1 < v2,
+                    _ => false
+                }
+            },
+            Condition::Eq(a, b) => {
+                match (variables.get(a), variables.get(b)) {
+                    (Some(v1), Some(v2)) => v1 == v2,
+                    _ => false
+                }
+            },
+        }
+    }
+}
+
+fn and_conditions(conditions: &[Condition], variables: &HashMap<String, f32>) -> bool {
+    for condition in conditions {
+        if !condition.eval(variables) {
+            return false;
+        }
+    }
+    return true;
+}
+
+struct Transition {
+    to: String,
+    conditions: Vec<Condition>
+}
+
+struct Node {
+    animation: Animation,
+    transitions: Vec<Transition>, 
+    has_exit_time: bool,
+}
+
+#[derive(Default)]
+pub struct NodeTree {
+    pub variables: HashMap<String, f32>,
+    pub nodes: HashMap<String, Node>,
+    active: Option<String>,
+}
+
+impl NodeTree {
+    fn recurse_transition(&mut self, is_last_frame: bool) {
+        if !self.transition(is_last_frame) {
+            return;
+        }
+        let mut transitions = 1;
+        while transitions < self.nodes.len() {
+            if !self.transition(is_last_frame) {
+                return;
+            }
+            transitions += 1;
+        }
+        panic!("recursive transition loop");
+    }
+
+    fn transition(&mut self, is_last_frame: bool) -> bool {
+        let active_name = unwrap_or_return!(self.active, false); 
+        let active = unwrap_or_return!(self.nodes.get(&active_name), false);
+        if !is_last_frame && active.has_exit_time {
+            return false;
+        }
+        for transition in active.transitions {
+            let should_transition = and_conditions(&transition.conditions, &self.variables);
+            if should_transition {
+                self.active = Some(transition.to);
+                return true;
+            }
+        }
+        return false;
+    }
+}
